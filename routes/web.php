76ce691c897033a3bd6c0728ed62e61694ed734a
sellayspy <149935917+sellayspy@users.noreply.github.com>
2025-05-24 22:20:23 +0700

    penambahan halaman login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/login', [UserController::class, 'login'])->name('login');
 Route::get('/', function () {
     return view('pages/dashboard/index');
 })->name('dashboard');

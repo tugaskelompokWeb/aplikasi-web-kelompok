1da5f3ab28620ed478032cbd26ea0699f9c6b615
sellayspy <149935917+sellayspy@users.noreply.github.com>
2025-05-24 22:23:43 +0700

    perbaikan halaman login

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login.form');
 Route::post('/login', [UserController::class, 'login'])->name('login');
 Route::get('/', function () {
     return view('pages/dashboard/index');

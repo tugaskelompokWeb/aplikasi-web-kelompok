450f5230c91971e287f57cfe682a3383748b6fef
Rizqi Siti Inayah 2428250080P <rizqisitiinayah_2428250080p@mhs.mdp.ac.id>
2025-05-26 12:16:37 +0700

    perbaikan kendaraancontroller

---
+++ app/Http/Controllers/KendaraanController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use App\Models\Mekanik;
+use Illuminate\Http\Request;
+
+class KendaraanController extends Controller
+{
+    public function index(){
+        $kendaraans = Kendaraan::with('role')->get();
+        return view('pages.kendaraan.index', compact('kendaraan'));
+    }
+
+    public function create(){
+        $roles = Role::all();
+        return view('pages.kendaraan.create', compact('roles'));
+    }
+
+    public function store(Request $request){
+        $validated = $request->validate([
+            'no_plat' => 'required',
+            'merk' => 'required|string',
+            'tipe' => 'required|string',
+            'warna' => 'required|string',
+            'tahun' => 'required',
+            'id_pelanggan' => 'required'
+        ]);
+
+        $kendaraans = Kendaraan::create([
+            'no_plat' => $validated['no_plat'],
+            'merk' => $validated['merk'],
+            'tipe' => $validated['tipe'],
+            'warna' => $validated['warna'],
+            'tahun' => $validated['tahun'],
+            'id_pelanggan' => $validated['id_pelanggan'],
+        ]);
+        return redirect()->route('kendaraan.index');
+    }
+
+    public function edit($id) {
+        $kendaraans = Kendaraan::findOrFail($id);
+        $roles = Role::all();
+
+        return view('pages.kendaraan.edit', compact(['kendaraan', 'roles']));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $kendaraans = Kendaraan::findOrFail($id);
+
+        $validated = $request->validate([
+            'no_plat' => 'required',
+            'merk' => 'required|string',
+            'tipe' => 'required|string',
+            'warna' => 'required|string',
+            'tahun' => 'required',
+            'id_pelanggan' => 'required'
+        ]);
+
+        $kendaraans->update($validated);
+        return redirect()->route('kendaraan.index')->with('Kendaraan berhasil di update');
+    }
+
+    public function destroy($id);
+    {
+        $kendaraans = Kendaraan::findOrFail($id);
+
+        if (auth()->id() === $kendaraans->$id) {
+            return redirect()->route('kendaraan.index')->with('error', 'Tidak dapat menghapus data.');
+        }
+
+        $kendaraans->delete();
+
+        return redirect()->route('kendaraan.index')->with('Kendaraan berhasil dihapus.');
+    }
+}

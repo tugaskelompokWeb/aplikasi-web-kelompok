9ab5657b9544b95c7462f7512e099ba12f4758ea
sellayspy <149935917+sellayspy@users.noreply.github.com>
2025-05-27 23:59:36 +0700

    Penambahan fitur login dan file database

--- db_projectuas.sql
+++ db_projectuas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 25 Bulan Mei 2025 pada 11.45
+-- Waktu pembuatan: 27 Bulan Mei 2025 pada 18.58
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -20,24 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_projectuas`
 --
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `barangs`
---
-
-CREATE TABLE `barangs` (
-  `id_barang` bigint(20) UNSIGNED NOT NULL,
-  `kode_barang` varchar(255) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `kategori` varchar(255) DEFAULT NULL,
-  `satuan` varchar(255) DEFAULT NULL,
-  `stok` int(11) NOT NULL,
-  `harga` decimal(10,2) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -70,8 +52,8 @@ CREATE TABLE `cache_locks` (
 --
 
 CREATE TABLE `detail_servis` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `id_servis` bigint(20) UNSIGNED NOT NULL,
+  `id` char(36) NOT NULL,
+  `servis_id` char(36) NOT NULL,
   `nama_jasa` varchar(255) NOT NULL,
   `biaya` decimal(10,2) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -92,25 +74,6 @@ CREATE TABLE `failed_jobs` (
   `payload` longtext NOT NULL,
   `exception` longtext NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `garansis`
---
-
-CREATE TABLE `garansis` (
-  `id_garansi` bigint(20) UNSIGNED NOT NULL,
-  `id_pelanggan` bigint(20) UNSIGNED NOT NULL,
-  `id_kendaraan` bigint(20) UNSIGNED NOT NULL,
-  `user_id` bigint(20) UNSIGNED NOT NULL,
-  `keluhan` text DEFAULT NULL,
-  `tanggal_garansi` date NOT NULL,
-  `batas_akhir` date NOT NULL,
-  `status` enum('aktif','kadaluarsa','batal') NOT NULL DEFAULT 'aktif',
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -151,42 +114,6 @@ CREATE TABLE `job_batches` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kendaraans`
---
-
-CREATE TABLE `kendaraans` (
-  `id_kendaraan` bigint(20) UNSIGNED NOT NULL,
-  `no_plat` varchar(255) NOT NULL,
-  `merek` varchar(255) DEFAULT NULL,
-  `tipe` varchar(255) NOT NULL,
-  `no_rangka` varchar(255) NOT NULL,
-  `no_mesin` varchar(255) DEFAULT NULL,
-  `warna` varchar(255) DEFAULT NULL,
-  `tahun` year(4) NOT NULL,
-  `id_pelanggan` bigint(20) UNSIGNED NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `mekaniks`
---
-
-CREATE TABLE `mekaniks` (
-  `id_mekanik` bigint(20) UNSIGNED NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `telepon` varchar(255) DEFAULT NULL,
-  `keahlian` varchar(255) DEFAULT NULL,
-  `status` enum('aktif','nonaktif') NOT NULL DEFAULT 'aktif',
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Struktur dari tabel `migrations`
 --
 
@@ -211,10 +138,11 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (8, '2025_05_20_145455_create_servis_table', 1),
 (9, '2025_05_20_145510_create_transaksis_table', 1),
 (10, '2025_05_20_145531_create_jasa_services_table', 1),
-(11, '2025_05_20_145544_create_garansis_table', 2),
-(12, '2025_05_23_013747_create_transaksi_items_table', 2),
-(13, '2025_05_25_035706_create_roles_table', 3),
-(14, '2025_05_25_035825_add_kolom_users_table', 3);
+(11, '2025_05_20_145544_create_garansis_table', 1),
+(12, '2025_05_23_013747_create_transaksi_items_table', 1),
+(13, '2025_05_25_035706_create_roles_table', 1),
+(14, '2025_05_25_035825_add_kolom_users_table', 1),
+(15, '2025_05_25_132029_drop_column_no_rangka_kendaraan_table', 1);
 
 -- --------------------------------------------------------
 
@@ -231,29 +159,11 @@ CREATE TABLE `password_reset_tokens` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pelanggans`
---
-
-CREATE TABLE `pelanggans` (
-  `id_pelanggan` bigint(20) UNSIGNED NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `alamat` text DEFAULT NULL,
-  `telepon` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `jenis_kelamin` enum('L','P') DEFAULT NULL,
-  `tanggal_lahir` date DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Struktur dari tabel `roles`
 --
 
 CREATE TABLE `roles` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+  `id` char(36) NOT NULL,
   `nama` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -264,9 +174,9 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `nama`, `created_at`, `updated_at`) VALUES
-(1, 'super-admin', NULL, NULL),
-(2, 'owner', NULL, NULL),
-(3, 'manager', NULL, NULL);
+('019711fc-ca40-73b4-b51e-a3745f67a55e', 'developer', '2025-05-27 06:44:28', '2025-05-27 06:44:28'),
+('019711fc-fc75-71b0-86ac-473f039c4025', 'admin', '2025-05-27 06:44:41', '2025-05-27 06:44:41'),
+('019711fd-278e-715a-84ec-c41141d774f7', 'owner', '2025-05-27 06:44:52', '2025-05-27 06:44:52');
 
 -- --------------------------------------------------------
 
@@ -275,14 +185,14 @@ INSERT INTO `roles` (`id`, `nama`, `created_at`, `updated_at`) VALUES
 --
 
 CREATE TABLE `servis` (
-  `id_servis` bigint(20) UNSIGNED NOT NULL,
+  `id` char(36) NOT NULL,
   `tgl_datang` date NOT NULL,
   `tgl_keluar` date DEFAULT NULL,
   `total_biaya` decimal(10,2) DEFAULT NULL,
   `keluhan_awal` text DEFAULT NULL,
   `status_servis` enum('proses','selesai','dibatalkan') NOT NULL DEFAULT 'proses',
-  `id_kendaraan` bigint(20) UNSIGNED NOT NULL,
-  `id_mekanik` bigint(20) UNSIGNED NOT NULL,
+  `kendaraan_id` char(36) NOT NULL,
+  `mekanik_id` char(36) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -305,27 +215,13 @@ CREATE TABLE `sessions` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `transaksis`
---
-
-CREATE TABLE `transaksis` (
-  `id_transaksi` bigint(20) UNSIGNED NOT NULL,
-  `id_servis` bigint(20) UNSIGNED NOT NULL,
-  `total_harga` decimal(10,2) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Struktur dari tabel `transaksi_items`
 --
 
 CREATE TABLE `transaksi_items` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `id_transaksi` bigint(20) UNSIGNED NOT NULL,
-  `id_barang` bigint(20) UNSIGNED NOT NULL,
+  `id` char(36) NOT NULL,
+  `transaksi_id` char(36) NOT NULL,
+  `barang_id` char(36) NOT NULL,
   `jumlah` int(11) NOT NULL,
   `harga_satuan` decimal(10,2) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -340,7 +236,7 @@ CREATE TABLE `transaksi_items` (
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `role_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `role_id` char(36) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
@@ -358,18 +254,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `role_id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `no_telp`, `posisi`, `alamat`) VALUES
-(1, 1, 'admin', 'admin@gmail.com', NULL, '$2y$12$vHnPo5gh5odNd79P6nfjfe22dYlkkslnOCGN2.If9dpJKNnYPC306', NULL, '2025-05-24 22:24:22', '2025-05-24 22:24:22', '08978886768768', 'developer', 'lrng Asoka,jln sukawinatan');
+(1, '019711fc-ca40-73b4-b51e-a3745f67a55e', 'developer', 'developer@gmail', NULL, '$2y$12$K/pXbEzdwXAaFxsk7/hFWO2xUbDr/9nrSVlQV6yDi.8KpLWUFHMX.', NULL, '2025-05-27 06:45:47', '2025-05-27 06:45:47', '987896434443', 'Developer', 'secret');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indeks untuk tabel `barangs`
---
-ALTER TABLE `barangs`
-  ADD PRIMARY KEY (`id_barang`),
-  ADD UNIQUE KEY `barangs_kode_barang_unique` (`kode_barang`);
 
 --
 -- Indeks untuk tabel `cache`
@@ -388,7 +277,7 @@ ALTER TABLE `cache_locks`
 --
 ALTER TABLE `detail_servis`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `detail_servis_id_servis_foreign` (`id_servis`);
+  ADD KEY `detail_servis_servis_id_foreign` (`servis_id`);
 
 --
 -- Indeks untuk tabel `failed_jobs`
@@ -396,15 +285,6 @@ ALTER TABLE `detail_servis`
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
-
---
--- Indeks untuk tabel `garansis`
---
-ALTER TABLE `garansis`
-  ADD PRIMARY KEY (`id_garansi`),
-  ADD KEY `garansis_id_pelanggan_foreign` (`id_pelanggan`),
-  ADD KEY `garansis_id_kendaraan_foreign` (`id_kendaraan`),
-  ADD KEY `garansis_user_id_foreign` (`user_id`);
 
 --
 -- Indeks untuk tabel `jobs`
@@ -420,20 +300,6 @@ ALTER TABLE `job_batches`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `kendaraans`
---
-ALTER TABLE `kendaraans`
-  ADD PRIMARY KEY (`id_kendaraan`),
-  ADD UNIQUE KEY `kendaraans_no_plat_unique` (`no_plat`),
-  ADD KEY `kendaraans_id_pelanggan_foreign` (`id_pelanggan`);
-
---
--- Indeks untuk tabel `mekaniks`
---
-ALTER TABLE `mekaniks`
-  ADD PRIMARY KEY (`id_mekanik`);
-
---
 -- Indeks untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
@@ -446,12 +312,6 @@ ALTER TABLE `password_reset_tokens`
   ADD PRIMARY KEY (`email`);
 
 --
--- Indeks untuk tabel `pelanggans`
---
-ALTER TABLE `pelanggans`
-  ADD PRIMARY KEY (`id_pelanggan`);
-
---
 -- Indeks untuk tabel `roles`
 --
 ALTER TABLE `roles`
@@ -462,9 +322,9 @@ ALTER TABLE `roles`
 -- Indeks untuk tabel `servis`
 --
 ALTER TABLE `servis`
-  ADD PRIMARY KEY (`id_servis`),
-  ADD KEY `servis_id_kendaraan_foreign` (`id_kendaraan`),
-  ADD KEY `servis_id_mekanik_foreign` (`id_mekanik`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `servis_kendaraan_id_foreign` (`kendaraan_id`),
+  ADD KEY `servis_mekanik_id_foreign` (`mekanik_id`);
 
 --
 -- Indeks untuk tabel `sessions`
@@ -475,19 +335,12 @@ ALTER TABLE `sessions`
   ADD KEY `sessions_last_activity_index` (`last_activity`);
 
 --
--- Indeks untuk tabel `transaksis`
---
-ALTER TABLE `transaksis`
-  ADD PRIMARY KEY (`id_transaksi`),
-  ADD KEY `transaksis_id_servis_foreign` (`id_servis`);
-
---
 -- Indeks untuk tabel `transaksi_items`
 --
 ALTER TABLE `transaksi_items`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `transaksi_items_id_transaksi_foreign` (`id_transaksi`),
-  ADD KEY `transaksi_items_id_barang_foreign` (`id_barang`);
+  ADD KEY `transaksi_items_transaksi_id_foreign` (`transaksi_id`),
+  ADD KEY `transaksi_items_barang_id_foreign` (`barang_id`);
 
 --
 -- Indeks untuk tabel `users`
@@ -502,28 +355,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT untuk tabel `barangs`
---
-ALTER TABLE `barangs`
-  MODIFY `id_barang` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `detail_servis`
---
-ALTER TABLE `detail_servis`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT untuk tabel `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `garansis`
---
-ALTER TABLE `garansis`
-  MODIFY `id_garansi` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `jobs`
@@ -532,58 +367,16 @@ ALTER TABLE `jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `kendaraans`
---
-ALTER TABLE `kendaraans`
-  MODIFY `id_kendaraan` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `mekaniks`
---
-ALTER TABLE `mekaniks`
-  MODIFY `id_mekanik` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT untuk tabel `pelanggans`
---
-ALTER TABLE `pelanggans`
-  MODIFY `id_pelanggan` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `roles`
---
-ALTER TABLE `roles`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT untuk tabel `servis`
---
-ALTER TABLE `servis`
-  MODIFY `id_servis` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `transaksis`
---
-ALTER TABLE `transaksis`
-  MODIFY `id_transaksi` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `transaksi_items`
---
-ALTER TABLE `transaksi_items`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -593,41 +386,21 @@ ALTER TABLE `users`
 -- Ketidakleluasaan untuk tabel `detail_servis`
 --
 ALTER TABLE `detail_servis`
-  ADD CONSTRAINT `detail_servis_id_servis_foreign` FOREIGN KEY (`id_servis`) REFERENCES `servis` (`id_servis`) ON DELETE CASCADE;
-
---
--- Ketidakleluasaan untuk tabel `garansis`
---
-ALTER TABLE `garansis`
-  ADD CONSTRAINT `garansis_id_kendaraan_foreign` FOREIGN KEY (`id_kendaraan`) REFERENCES `kendaraans` (`id_kendaraan`) ON DELETE CASCADE,
-  ADD CONSTRAINT `garansis_id_pelanggan_foreign` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggans` (`id_pelanggan`) ON DELETE CASCADE,
-  ADD CONSTRAINT `garansis_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
-
---
--- Ketidakleluasaan untuk tabel `kendaraans`
---
-ALTER TABLE `kendaraans`
-  ADD CONSTRAINT `kendaraans_id_pelanggan_foreign` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggans` (`id_pelanggan`) ON DELETE CASCADE;
+  ADD CONSTRAINT `detail_servis_servis_id_foreign` FOREIGN KEY (`servis_id`) REFERENCES `servis` (`id`) ON DELETE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `servis`
 --
 ALTER TABLE `servis`
-  ADD CONSTRAINT `servis_id_kendaraan_foreign` FOREIGN KEY (`id_kendaraan`) REFERENCES `kendaraans` (`id_kendaraan`) ON DELETE CASCADE,
-  ADD CONSTRAINT `servis_id_mekanik_foreign` FOREIGN KEY (`id_mekanik`) REFERENCES `mekaniks` (`id_mekanik`) ON DELETE CASCADE;
-
---
--- Ketidakleluasaan untuk tabel `transaksis`
---
-ALTER TABLE `transaksis`
-  ADD CONSTRAINT `transaksis_id_servis_foreign` FOREIGN KEY (`id_servis`) REFERENCES `servis` (`id_servis`) ON DELETE CASCADE;
+  ADD CONSTRAINT `servis_kendaraan_id_foreign` FOREIGN KEY (`kendaraan_id`) REFERENCES `kendaraan` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `servis_mekanik_id_foreign` FOREIGN KEY (`mekanik_id`) REFERENCES `mekanik` (`id`) ON DELETE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `transaksi_items`
 --
 ALTER TABLE `transaksi_items`
-  ADD CONSTRAINT `transaksi_items_id_barang_foreign` FOREIGN KEY (`id_barang`) REFERENCES `barangs` (`id_barang`) ON DELETE CASCADE,
-  ADD CONSTRAINT `transaksi_items_id_transaksi_foreign` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksis` (`id_transaksi`) ON DELETE CASCADE;
+  ADD CONSTRAINT `transaksi_items_barang_id_foreign` FOREIGN KEY (`barang_id`) REFERENCES `barang` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `transaksi_items_transaksi_id_foreign` FOREIGN KEY (`transaksi_id`) REFERENCES `transaksi` (`id`) ON DELETE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `users`

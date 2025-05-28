8bd5b589974bdb0aea508cb4b45d1a127b6e1cc6
Steven <stevenjunius30@gmail.com>
2025-05-28 23:57:55 +0700

    data barang di model

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -3,8 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 
 class Barang extends Model
 {
+    use HasFactory;
 
+    protected $table = 'barang';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'id',
+        'kode_barang',
+        'nama',
+        'kategori',
+        'satuan',
+        'stok',
+        'harga'
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
 }
